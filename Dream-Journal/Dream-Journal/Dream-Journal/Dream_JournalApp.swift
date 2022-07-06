5c5e258b9acef55fbf6ecdec0a2415d8bca11174
kathleendiep <kathleendiep@gmail.com>
2022-07-06 10:06:26 -0700

    Added folder

---
+++ Dream-Journal/Dream-Journal/Dream-Journal/Dream_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dream_JournalApp.swift
+//  Dream-Journal
+//
+//  Created by Kathleen Diep on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Dream_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
